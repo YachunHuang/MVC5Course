0c6da63804faeb6d2a8aae0cf0934fbe71497c8a
Yachun Huang <yachun@edetw.com>
2016-03-17 17:33:21 +0800

    新手上路初體驗

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -8,15 +8,66 @@ namespace MVC5Course.Controllers
 {
     public class HomeController : Controller
     {
+        //TODO:輸入propfull按下tab
+        //private int myprop;
+
+        //public int MyProperty
+        //{
+        //    get { return myprop; }
+        //    set { myprop = value; }
+        //}
+
         public ActionResult Index()
         {
+            //TODO: 工作清單demo
+
+            //TODO: enum and switch demo
+            //ConsoleColor mycolor = ConsoleColor.Blue;
+            //switch (mycolor)
+            //{
+            //    case ConsoleColor.Black:
+            //        break;
+            //    case ConsoleColor.DarkBlue:
+            //        break;
+            //    case ConsoleColor.DarkGreen:
+            //        break;
+            //    case ConsoleColor.DarkCyan:
+            //        break;
+            //    case ConsoleColor.DarkRed:
+            //        break;
+            //    case ConsoleColor.DarkMagenta:
+            //        break;
+            //    case ConsoleColor.DarkYellow:
+            //        break;
+            //    case ConsoleColor.Gray:
+            //        break;
+            //    case ConsoleColor.DarkGray:
+            //        break;
+            //    case ConsoleColor.Blue:
+            //        break;
+            //    case ConsoleColor.Green:
+            //        break;
+            //    case ConsoleColor.Cyan:
+            //        break;
+            //    case ConsoleColor.Red:
+            //        break;
+            //    case ConsoleColor.Magenta:
+            //        break;
+            //    case ConsoleColor.Yellow:
+            //        break;
+            //    case ConsoleColor.White:
+            //        break;
+            //    default:
+            //        break;
+            //}
+
             return View();
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+            
             return View();
         }
 
@@ -24,6 +75,11 @@ namespace MVC5Course.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Test()
+        {
             return View();
         }
     }
